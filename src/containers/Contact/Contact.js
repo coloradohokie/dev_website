30fe1734af32968e02ff1a1e870b76d3791fd21e
Michael Newman <coloradohokie@gmail.com>
2020-06-12 22:41:01 -0600

    add profile photo; update contact page

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import classes from './Contact.module.css'
 
 const contact = () => (
     <div>
         <h1>contact</h1>
-        <p>This is the contact page.</p>
+        <p>To inquire about professional work, please contact me via LinkedIn.com</p>
+        <p>You may also fill out the form below.</p>
+
+        <div className={classes.ContactForm}>
+            <form>
+                <input type="text" name="name" placeholder="Name" />
+                <input type="email" name="email" placeholder="Email" />
+                <input type="text" name="company" placeholder="Company/Organization" />
+                <input type="text" name="phone" placeholder="Phone Number" />
+                <textarea name="message" placheolder="Please enter your message"></textarea>
+                <button>Send</button>
+            </form>
+        </div>
     </div>
 )
 
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import classes from './Home.module.css'
+import headshot from '../../assets/profile/headshot.jpg'
 
 const home = () => (
     <div>
         <h1>About</h1>
-        <p>Hello, my name is Michael Newman and I am a former business analyst, project manager, and product owner turned full stack web developer.</p>
-        <p>I have worked in IT for over 15 years in numerous industries at companies of all sizes, with an emphasis on user experience. My passion is creating products that just work. After building a career of almost two decades do this, I decided I wanted to take a shot at coding. I said goodbye to my last contract gig just before the holidays in 2019, and in January 2020 I enrolled in Flatiron School's software development coding bootcamp.</p>
+        <div className={classes.Home}>
+            <div className={classes.Headshot}>
+                <img src={headshot} alt="Michael Newman" />
+            </div>
 
-        <p>As the world has shifted to mobile browsing, smartphone apps, and the Internet of Things I've put my emphasis there as well. My focus has been on React and Ruby for web browsing, and a bit of Java for androidOS. I do still hope to pick up C++ and C# in the days ahead.</p>
+            <div className={classes.VertLine}></div>
+
+            <div>
+                <p>Hello, my name is Michael Newman and I am a former business analyst, project manager, and product owner turned full stack web developer.</p>
+                <p>I have worked in IT for over 15 years in numerous industries at companies of all sizes, with an emphasis on user experience. My passion is creating products that just work. After building a career of almost two decades do this, I decided I wanted to take a shot at coding. I said goodbye to my last contract gig just before the holidays in 2019, and in January 2020 I enrolled in Flatiron School's software development coding bootcamp.</p>
+
+                <p>As the world has shifted to mobile browsing, smartphone apps, and the Internet of Things I've put my emphasis there as well. My focus has been on React and Ruby for web browsing, and a bit of Java for androidOS. I do still hope to pick up C++ and C# in the days ahead.</p>
+            </div>
+        </div>
         <div className={classes.line}></div>
     </div>
 )
